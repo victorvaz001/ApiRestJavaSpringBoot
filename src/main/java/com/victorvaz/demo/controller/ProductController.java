53feac43a2e5a1c2ae4556ac6e966dc058d5052d
Victor Vaz <victorvaz001@gmail.com>
2023-07-31 08:05:40 -0300

    Criação da Entidade Department e o ProductController

---
+++ src/main/java/com/victorvaz/demo/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.victorvaz.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.victorvaz.demo.entities.Department;
+
+@RestController //Configurarada para responder na Api
+@RequestMapping(value = "/products")
+public class ProductController {
+	
+	
+	@GetMapping //Configura o metodo para que ele responda na API Rest
+	public Department getObjects() {
+		Department d1 = new Department();
+		d1.setId(1L);
+		d1.setName("Tech");
+		
+		return d1;
+	}
+
+}
---
+++ src/main/java/com/victorvaz/demo/entities/Department.java
@@ -0,0 +1,52 @@
+package com.victorvaz.demo.entities;
+
+import java.util.Objects;
+
+public class Department {
+
+	private Long id;
+	private String name;
+
+	public Department() {
+
+	}
+
+	public Department(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		return Objects.equals(id, other.id);
+	}
+}
